b493a1cca0b096ae9bb64caaafc15632bb6049c2
deanwallflower <dean@deanwallflower.com>
2022-02-21 20:00:53 +0100

    Rotation and name-change features

--- app.ts
+++ app.ts
@@ -1,7 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   const player1Grid = document.getElementById("player-1-field") as HTMLElement;
   const player2Grid = document.getElementById("player-2-field") as HTMLElement;
-
+  const rotateButton = document.getElementById("rotate-button") as HTMLElement;
+  const shipsContainer = document.getElementById("player-1-ships") as HTMLElement;
+  const shipsContainerClass = shipsContainer.className;
+  const nameChangeButton = document.getElementById("name-change-button") as HTMLElement;
+  var playerName = document.getElementById("player-1-name") as HTMLElement;
+  var playerNameOrig = "CADET";
+  var noticeText = document.getElementById("update") as HTMLElement;
+  
   const gridChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
 
   for (let i = 0; i < gridChars.length; i++) {
@@ -20,4 +27,69 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     
   }
+
+  function rotateShip() {
+    shipsContainer.classList.toggle(`${shipsContainerClass}-vertical`);
+
+    const ships = shipsContainer?.children as HTMLCollection;
+    const shipsArray = Array.from(ships);
+
+    shipsArray.forEach((ship) => {
+      const shipSpecificClass = ship.className.split(" ")[1]
+      ship.classList.toggle(`${shipSpecificClass}-vertical`);
+    })
+  }
+
+  rotateButton.addEventListener("click", rotateShip)
+  
+  function changeName() {
+    var userVersion = playerName.innerHTML;
+
+    if (userVersion.length == 0) {
+
+      playerName.innerHTML = playerNameOrig;
+      noticeText.innerHTML = "try again";
+
+      } else {
+
+        localStorage.userEdits = userVersion;
+        noticeText.innerHTML = "name changed";
+
+      }
+    }
+
+    function showButton() {
+      nameChangeButton.classList.add("name-change-button-show")
+      noticeText.classList.add("update-hide")
+    }
+    function hideButton() {
+      nameChangeButton.classList.remove("name-change-button-show")
+      noticeText.classList.remove("update-hide")
+    }
+
+    nameChangeButton.addEventListener("click", changeName)
+    playerName.addEventListener("click", showButton)
+    nameChangeButton.addEventListener("click", hideButton)
+
+    
+    // window.onclick = function(event) {
+    //   if  (!event.target.classList.contains("mainButton") &&
+    //     dropdown.classList.contains("show")
+    //   ) {
+    //     dropdown.classList.remove("show");
+    //   }
+    // }
+
+
+  //   window.onload = () => {
+  //     var modal = document.getElementById('player-1-name') as HTMLElement;
+  //     // When the user clicks anywhere outside of the modal, save name
+  //     window.addEventListener('click', function (event) {
+  //         if (event.target !== modal) {
+  //           console.log("outside");
+  //             changeName();
+  //         }
+  //     });
+  // }
+
 });
