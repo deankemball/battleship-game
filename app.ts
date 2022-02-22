cb2fcb2ce0adaa593f46e06c7242e6b62a0309e3
deanwallflower <dean@deanwallflower.com>
2022-02-22 18:41:16 +0100

    Refactor rotation functionality, color change

--- app.ts
+++ app.ts
@@ -1,16 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // grid constants and types
   const player1Grid = document.getElementById("player-1-field") as HTMLElement;
   const player2Grid = document.getElementById("player-2-field") as HTMLElement;
+  const gridChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+
+  type Position = `${string}-${number}`
+  type PossibleValue = "" | ShipType;
+  type Grid = Record<Position, PossibleValue>;
+
+  const player1GridState: Grid = {};
+  const player2GridState: Grid = {};
+  
+  // ships constants
   const rotateButton = document.getElementById("rotate-button") as HTMLElement;
   const shipsContainer = document.getElementById("player-1-ships") as HTMLElement;
   const shipsContainerClass = shipsContainer.className;
+
+  // name change constants
   const nameChangeButton = document.getElementById("name-change-button") as HTMLElement;
   var playerName = document.getElementById("player-1-name") as HTMLElement;
   var playerNameOrig = "CADET";
   var noticeText = document.getElementById("update") as HTMLElement;
   
-  const gridChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
-
+  // grid functions
+  // create unique grid ids
   for (let i = 0; i < gridChars.length; i++) {
     for (let j = 1; j <= 10; j++) {
       const player1Id = `player-1-${gridChars[i]}-${j}`
@@ -25,23 +38,109 @@ document.addEventListener("DOMContentLoaded", () => {
       player1Grid.appendChild(player1Square);
       player2Grid.appendChild(player2Square);
     }
-    
   }
 
-  function rotateShip() {
-    shipsContainer.classList.toggle(`${shipsContainerClass}-vertical`);
-
-    const ships = shipsContainer?.children as HTMLCollection;
-    const shipsArray = Array.from(ships);
-
-    shipsArray.forEach((ship) => {
-      const shipSpecificClass = ship.className.split(" ")[1]
-      ship.classList.toggle(`${shipSpecificClass}-vertical`);
-    })
+  // create objects with unique key names (positions) and values (initialized as empty: "")
+  // create the key names
+  function createKeyValuePair(grid: Grid, position: Position, value: PossibleValue) {
+    grid[position] = value;
   }
-
-  rotateButton.addEventListener("click", rotateShip)
   
+  // return the value of the called key name 
+  function getValue(grid: Grid, position: Position) {
+    return grid[position];
+  }
+  
+  // loop through the grid and for every cell assign the unique key name and value (e.g. "b-3", "")
+  for (let i = 0; i < gridChars.length; i++) {
+    for (let j = 0; j <= 10; j++) {
+      createKeyValuePair(player1GridState, `${gridChars[i]}-${j}`, "");
+      createKeyValuePair(player2GridState, `${gridChars[i]}-${j}`, "");
+    }
+  }
+
+
+  // declare the type ShipType which is a union of all the ship sizes
+  type ShipType = 
+  | "destroyer" 
+  | "submarine" 
+  | "cruiser" 
+  | "battleship" 
+  | "carrier"
+
+  // declare the Ship class which has default attributes; type, isHorizontal, length
+  class Ship {
+    type: ShipType;
+    isHorizontal: boolean = true;
+    length: number;
+
+    // assign lenghts for each instance of ShipType
+    constructor(type: ShipType) {
+        this.type = type;
+          switch (this.type) {
+            case "destroyer": 
+              this.length = 2
+              break;
+            case "cruiser":
+            case "submarine":
+              this.length = 3
+              break;
+            case "battleship": 
+              this.length = 4
+              break;
+            case "carrier": 
+              this.length = 5
+              break;
+          }
+        }
+    }
+
+    // modifies the PlayerShip ShipType by adding combining the previous type from Ship with "-container"
+    class PlayerShip extends Ship {
+      element: HTMLElement
+      constructor(type: ShipType) {
+        super(type);
+        this.element = document.querySelector(
+          `.${this.type}-container`
+          ) as HTMLElement;
+        }
+      
+      rotate() {
+        this.isHorizontal = this.isHorizontal ? false : true;
+  
+        const shipSpecificClass = this.element.className.split(" ")[1];
+        this.element.classList.toggle(`${shipSpecificClass}-vertical`)
+      }
+    }
+
+    const shipsArray = [
+      new PlayerShip("destroyer"),
+      new PlayerShip("submarine"),
+      new PlayerShip("cruiser"),
+      new PlayerShip("battleship"),
+      new PlayerShip("carrier"),
+    ];
+    
+    const shipsContainerArray = [
+      "player-1-ships"
+    ];
+    
+    rotateButton.addEventListener("click", () =>
+      {
+        shipsContainer.classList.toggle(`${shipsContainerClass}-vertical`);
+        return shipsArray.forEach((ship) => ship.rotate());
+      }
+    )
+
+  // NAME CHANGE BUTTON
+  function showButton() {
+    nameChangeButton.classList.add("name-change-button-show")
+    noticeText.classList.add("update-hide")
+  }
+  function hideButton() {
+    nameChangeButton.classList.remove("name-change-button-show")
+    noticeText.classList.remove("update-hide")
+  }
   function changeName() {
     var userVersion = playerName.innerHTML;
 
@@ -54,42 +153,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
         localStorage.userEdits = userVersion;
         noticeText.innerHTML = "name changed";
-
+        setTimeout(function() {noticeText.classList.add("update-hide")}, 1500)
       }
     }
 
-    function showButton() {
-      nameChangeButton.classList.add("name-change-button-show")
-      noticeText.classList.add("update-hide")
-    }
-    function hideButton() {
-      nameChangeButton.classList.remove("name-change-button-show")
-      noticeText.classList.remove("update-hide")
-    }
 
     nameChangeButton.addEventListener("click", changeName)
     playerName.addEventListener("click", showButton)
     nameChangeButton.addEventListener("click", hideButton)
-
-    
-    // window.onclick = function(event) {
-    //   if  (!event.target.classList.contains("mainButton") &&
-    //     dropdown.classList.contains("show")
-    //   ) {
-    //     dropdown.classList.remove("show");
-    //   }
-    // }
-
-
-  //   window.onload = () => {
-  //     var modal = document.getElementById('player-1-name') as HTMLElement;
-  //     // When the user clicks anywhere outside of the modal, save name
-  //     window.addEventListener('click', function (event) {
-  //         if (event.target !== modal) {
-  //           console.log("outside");
-  //             changeName();
-  //         }
-  //     });
-  // }
 
 });
--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,22 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", () => {
+    // grid constants and types
     const player1Grid = document.getElementById("player-1-field");
     const player2Grid = document.getElementById("player-2-field");
+    const gridChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+    const player1GridState = {};
+    const player2GridState = {};
+    // ships constants
     const rotateButton = document.getElementById("rotate-button");
     const shipsContainer = document.getElementById("player-1-ships");
     const shipsContainerClass = shipsContainer.className;
+    // name change constants
     const nameChangeButton = document.getElementById("name-change-button");
     var playerName = document.getElementById("player-1-name");
     var playerNameOrig = "CADET";
     var noticeText = document.getElementById("update");
-    const gridChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+    // grid functions
+    // create unique grid ids
     for (let i = 0; i < gridChars.length; i++) {
         for (let j = 1; j <= 10; j++) {
             const player1Id = `player-1-${gridChars[i]}-${j}`;
@@ -22,16 +29,80 @@ document.addEventListener("DOMContentLoaded", () => {
             player2Grid.appendChild(player2Square);
         }
     }
-    function rotateShip() {
-        shipsContainer.classList.toggle(`${shipsContainerClass}-vertical`);
-        const ships = shipsContainer === null || shipsContainer === void 0 ? void 0 : shipsContainer.children;
-        const shipsArray = Array.from(ships);
-        shipsArray.forEach((ship) => {
-            const shipSpecificClass = ship.className.split(" ")[1];
-            ship.classList.toggle(`${shipSpecificClass}-vertical`);
-        });
+    // create objects with unique key names (positions) and values (initialized as empty: "")
+    // create the key names
+    function createKeyValuePair(grid, position, value) {
+        grid[position] = value;
     }
-    rotateButton.addEventListener("click", rotateShip);
+    // return the value of the called key name 
+    function getValue(grid, position) {
+        return grid[position];
+    }
+    // loop through the grid and for every cell assign the unique key name and value (e.g. "b-3", "")
+    for (let i = 0; i < gridChars.length; i++) {
+        for (let j = 0; j <= 10; j++) {
+            createKeyValuePair(player1GridState, `${gridChars[i]}-${j}`, "");
+            createKeyValuePair(player2GridState, `${gridChars[i]}-${j}`, "");
+        }
+    }
+    // declare the Ship class which has default attributes; type, isHorizontal, length
+    class Ship {
+        // assign lenghts for each instance of ShipType
+        constructor(type) {
+            this.isHorizontal = true;
+            this.type = type;
+            switch (this.type) {
+                case "destroyer":
+                    this.length = 2;
+                    break;
+                case "cruiser":
+                case "submarine":
+                    this.length = 3;
+                    break;
+                case "battleship":
+                    this.length = 4;
+                    break;
+                case "carrier":
+                    this.length = 5;
+                    break;
+            }
+        }
+    }
+    // modifies the PlayerShip ShipType by adding combining the previous type from Ship with "-container"
+    class PlayerShip extends Ship {
+        constructor(type) {
+            super(type);
+            this.element = document.querySelector(`.${this.type}-container`);
+        }
+        rotate() {
+            this.isHorizontal = this.isHorizontal ? false : true;
+            const shipSpecificClass = this.element.className.split(" ")[1];
+            this.element.classList.toggle(`${shipSpecificClass}-vertical`);
+        }
+    }
+    const shipsArray = [
+        new PlayerShip("destroyer"),
+        new PlayerShip("submarine"),
+        new PlayerShip("cruiser"),
+        new PlayerShip("battleship"),
+        new PlayerShip("carrier"),
+    ];
+    const shipsContainerArray = [
+        "player-1-ships"
+    ];
+    rotateButton.addEventListener("click", () => {
+        shipsContainer.classList.toggle(`${shipsContainerClass}-vertical`);
+        return shipsArray.forEach((ship) => ship.rotate());
+    });
+    // NAME CHANGE BUTTON
+    function showButton() {
+        nameChangeButton.classList.add("name-change-button-show");
+        noticeText.classList.add("update-hide");
+    }
+    function hideButton() {
+        nameChangeButton.classList.remove("name-change-button-show");
+        noticeText.classList.remove("update-hide");
+    }
     function changeName() {
         var userVersion = playerName.innerHTML;
         if (userVersion.length == 0) {
@@ -41,34 +112,10 @@ document.addEventListener("DOMContentLoaded", () => {
         else {
             localStorage.userEdits = userVersion;
             noticeText.innerHTML = "name changed";
+            setTimeout(function () { noticeText.classList.add("update-hide"); }, 1500);
         }
-    }
-    function showButton() {
-        nameChangeButton.classList.add("name-change-button-show");
-        noticeText.classList.add("update-hide");
-    }
-    function hideButton() {
-        nameChangeButton.classList.remove("name-change-button-show");
-        noticeText.classList.remove("update-hide");
     }
     nameChangeButton.addEventListener("click", changeName);
     playerName.addEventListener("click", showButton);
     nameChangeButton.addEventListener("click", hideButton);
-    // window.onclick = function(event) {
-    //   if  (!event.target.classList.contains("mainButton") &&
-    //     dropdown.classList.contains("show")
-    //   ) {
-    //     dropdown.classList.remove("show");
-    //   }
-    // }
-    //   window.onload = () => {
-    //     var modal = document.getElementById('player-1-name') as HTMLElement;
-    //     // When the user clicks anywhere outside of the modal, save name
-    //     window.addEventListener('click', function (event) {
-    //         if (event.target !== modal) {
-    //           console.log("outside");
-    //             changeName();
-    //         }
-    //     });
-    // }
 });
